5a148018ee133f6445910b6350d97da7767a383f
Aleksandr Kamenskii <kamenskogo@gmail.com>
2024-01-30 18:41:44 +0100

    [persistence]: implemented DAO

--- src/main.rs
+++ src/main.rs
@@ -2,15 +2,35 @@ use axum::{
     routing::{delete, get, post},
     Router,
 };
+use dotenvy::dotenv;
+use log::{info};
+use sqlx::postgres::PgPoolOptions;
 use tokio::net::TcpListener;
 
 mod handlers;
 mod models;
+mod persistence;
 
 use handlers::*;
 
 #[tokio::main]
 async fn main() {
+    pretty_env_logger::init();
+    dotenv().ok();
+    let pool = PgPoolOptions::new()
+        .max_connections(5)
+        .connect(&std::env::var("DATABASE_URL").expect("DATABASE_URL must be set."))
+        .await
+        .expect("Failed to create Postgres connection pool!");
+
+    let recs = sqlx::query!("SELECT * FROM questions")
+        .fetch_all(&pool)
+        .await
+        .unwrap();
+
+    info!("********* Question Records *********");
+    info!("{:?}", recs);
+
     let app = Router::new()
         .route("/question", post(create_question))
         .route("/questions", get(read_questions))
@@ -20,5 +40,7 @@ async fn main() {
         .route("/answer", delete(delete_answer));
 
     let listener = TcpListener::bind("127.0.0.1:8000").await.unwrap();
-    axum::serve(listener, app.into_make_service()).await.unwrap();
+    axum::serve(listener, app.into_make_service())
+        .await
+        .unwrap();
 }
